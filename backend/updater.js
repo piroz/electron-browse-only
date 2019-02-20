af5a715aa0b56e5ebd40e076c99dbc6989d8d5c5
Hiroshi Murakami <piroz@users.noreply.github.com>
2019-02-20 15:30:49 +0900

    fix updater

--- backend/updater.js
+++ backend/updater.js
@@ -20,6 +20,7 @@ function sendStatus(msg) {
 function startUpdater() {
 
     autoUpdater.logger = getLogger();
+    autoUpdater.autoInstallOnAppQuit = true;
 
     autoUpdater.on("checking-for-update", () => {
         sendStatus("アプリの更新版を確認中");
@@ -33,14 +34,14 @@ function startUpdater() {
     autoUpdater.on("error", (err) => {
         sendStatus("Error in auto-updater. " + err);
     })
-    autoUpdater.on("download-progress", (progressObj) => {
+    /*autoUpdater.on("download-progress", (progressObj) => {
         let log_message = "Download speed: " + progressObj.bytesPerSecond;
         log_message = log_message + " - Downloaded " + progressObj.percent + "%";
         log_message = log_message + " (" + progressObj.transferred + "/" + progressObj.total + ")";
         sendStatus(log_message);
-    })
+    })*/
     autoUpdater.on("update-downloaded", (info) => {
-        sendStatus("アプリの更新版がダウンロードできました");
+        sendStatus("アプリの更新版がダウンロードできました、次回起動時に更新されます。");
     });
 
     autoUpdater.checkForUpdatesAndNotify();
